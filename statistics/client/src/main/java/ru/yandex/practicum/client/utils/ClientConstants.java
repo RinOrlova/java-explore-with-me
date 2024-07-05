e0435d6b41f25c6b4c126f50856968c99030c4e6
iorlova <rinorlova@gmail.com>
2024-07-05 20:50:59 +0200

    [feature] add main logic

---
+++ statistics/client/src/main/java/ru/yandex/practicum/client/utils/ClientConstants.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.client.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ClientConstants {
+    public static final String START_REQUEST_PARAM = "?start=";
+    public static final String END_REQUEST_PARAM = "&end=";
+    public static final String URIS_REQUEST_PARAM = "&uris=";
+    public static final String UNIQUE_REQUEST_PARAM = "&unique=";
+}
--- statistics/common-dto/src/main/java/ru/yandex/practicum/common/dto/HitRequest.java
+++ statistics/common-dto/src/main/java/ru/yandex/practicum/common/dto/HitRequest.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,6 @@ public class HitRequest {
     private String app;
     private String uri;
     private String ip;
+    @JsonIgnore
     private final LocalDateTime timestamp = LocalDateTime.now();
 }
